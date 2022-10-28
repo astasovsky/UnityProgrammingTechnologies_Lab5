61075e999d3b4cf2378ea7c0a28e1dd7cdc4b1a0
astasovsky <stasovsky.ua@gmail.com>
2022-10-28 19:59:02 +0300

    Task 5.1 Clicky Mouse. Done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Target[] targets;
+
+    private const float SpawnRate = 1;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            int index = Random.Range(0, targets.Length);
+            Instantiate(targets[index]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private const float MinSpeed = 14;
+    private const float MaxSpeed = 17;
+    private const float MaxTorque = 4;
+    private const float XRange = 4;
+    private const float YSpawnPosition = -6;
+
+    private Rigidbody _targetRigidbody;
+
+    private void Awake()
+    {
+        _targetRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        transform.position = RandomSpawnPos();
+        _targetRigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _targetRigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private static Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(MinSpeed, MaxSpeed);
+    }
+
+    private static float RandomTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
+    }
+
+    private static Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-XRange, XRange), YSpawnPosition);
+    }
+}
